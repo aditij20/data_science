a356420dc5b90e127d213a7c0ed10eb4d8bcd6a1
Aditi Joshi <aditi7@gmail.com>
2013-06-21 19:05:21 -0400

    quick plot of smoking

---
+++ ggplot_assignment/Smoking.r
@@ -0,0 +1,20 @@
+
+setwd("~/my_homework/ggplot_assignment")
+
+smoking <- read.csv ("training_SyncPatientSmokingStatus.csv")
+smoking_status <- read.csv ("SyncSmokingStatus.csv")
+
+smokingtotal <- merge (smoking, smoking_status)
+
+# 4,427 unique patients from 4,940 total rows
+# possible future todo: select only most recent
+# for each patient.
+
+library(ggplot2)
+smoking_plot <- ggplot (data = smokingtotal) +
+  aes (x = Description) + geom_bar() +
+  coord_flip() + xlab("Frequency")
+
+png("smoking_plot.png", width=800, height=480)
+smoking_plot
+dev.off()
